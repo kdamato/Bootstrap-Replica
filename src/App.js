53f0aed588c65098fbf1f1a3a09ec352c749aa57
Kyle DAmato <kyledamato4@gmail.com>
2022-08-10 21:28:14 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import TopBar from './components/TopBar';
+import StoreListing from './components/StoreListing';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <TopBar />
+      <StoreListing />
     </div>
   );
 }
